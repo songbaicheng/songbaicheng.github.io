7fddb7785c61f77c8eef52962d0114173ba1a1c4
songbaicheng <2524218694@qq.com>
2023-06-01 15:58:08 +0800

    modify sidebar part

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -5,9 +5,9 @@ export const Navbar = navbar([
     { text: "演示", icon: "discover", link: "/zh/demo/" },
     { text: "学习之路", icon: "study", link: "/study/" },
     { text: "工作任务", icon: "workingDirectory", link: "/work-task/" },
-    { text: "书籍推荐", icon: "time", link: "/books/" },
+    { text: "书籍推荐", icon: "repo", link: "/books/" },
     {
-        text: "过去", icon: "past", children: [{
+        text: "过去", icon: "time", children: [{
             text: "阿里云",
             link: "http://47.93.240.83:8000/"
         }, {
--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -2,7 +2,6 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const Sidebar = sidebar({
   "/": [
-    "",
     {
       text: "如何使用",
       icon: "creative",
@@ -19,4 +18,58 @@ export const Sidebar = sidebar({
     "intro",
     "slides",
   ],
+  '/study/': [
+    {
+      text: 'Java',
+      collapsible: true,
+      children: ['/study/backend/java/dynamic-proxy.md']
+    },
+    {
+      text: '前端技术',
+      collapsible: true,
+      children: ['/study/frontend/README.md']
+    },
+    {
+      text: '设计模式',
+      collapsible: true,
+      children: [
+        '/study/design-pattern/README.md',
+        '/study/design-pattern/observer.md'
+      ]
+    },
+    {
+      text: '算法',
+      collapsible: true,
+      children: ['/study/ads/algorithms/README.md']
+    },
+    {
+      text: '数据结构',
+      collapsible: true,
+      children: ['/study/ads/data-structure/README.md']
+    }
+  ],
+  '/work-task/': [
+    {
+      text: '开发任务',
+      collapsible: true,
+      children: [
+        '/work-task/development/ant.md',
+        '/work-task/development/scheduled.md',
+        '/work-task/development/log-desensitization.md',
+        '/work-task/development/reflection.md'
+      ]
+    },
+    {
+      text: '设计任务',
+      collapsible: true,
+      children: [
+        '/work-task/design/cmds.md',
+        '/work-task/design/migrate-svn2git.md'
+      ]
+    }
+  ],
+  '/books/': [
+    '/books/java.md',
+    '/books/other-skills.md'
+  ]
 });
