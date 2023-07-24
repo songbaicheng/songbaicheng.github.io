9271c5fda9443f5771ef16263dab8916c2247522
songbaicheng <57879271+songbaicheng@users.noreply.github.com>
2023-07-24 08:18:01 +0000

    Deploying to gh-pages from @ songbaicheng/songbaicheng.github.io@2790030c6df6d07f15acca00d19fa3217c2f8683 🚀

---
+++ assets/english.html-c14ee9d6.js
@@ -0,0 +1,59 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,e as s}from"./app-7e92a49c.js";const a={},l=s(`<h1 id="english" tabindex="-1"><a class="header-anchor" href="#english" aria-hidden="true">#</a> English</h1><h2 id="简单句" tabindex="-1"><a class="header-anchor" href="#简单句" aria-hidden="true">#</a> 简单句</h2><p>我们从最简单的结构开始，一个句子只要有物质和运动就可以把一件事情最根本的要素描述出来，即 n. + v. 也就是一个名词和一个动词构成主<sup class="footnote-ref"><a href="#footnote1">[1]</a><a class="footnote-anchor" id="footnote-ref1"></a></sup>谓<sup class="footnote-ref"><a href="#footnote2">[2]</a><a class="footnote-anchor" id="footnote-ref2"></a></sup>，而且是一对一对应，一个主语对应一个谓语。在简单句的变化都是谓语的变化，至于如何变化我们得先知道谓语的分类</p><ul><li>谓语动词（v.）的分类 <ul><li>有实意 <ul><li>不及物动词（vi.）：主语独立完成，不用借助其他物就可以组成主谓的句子。</li><li>及物动词（vt.）：主语不能独立完成，需要借助宾语<sup class="footnote-ref"><a href="#footnote3">[3]</a><a class="footnote-anchor" id="footnote-ref3"></a></sup>来完成，构成主谓宾、主谓双宾<sup class="footnote-ref"><a href="#footnote4">[4]</a><a class="footnote-anchor" id="footnote-ref4"></a></sup>和主谓宾补<sup class="footnote-ref"><a href="#footnote5">[5]</a><a class="footnote-anchor" id="footnote-ref5"></a></sup>等结构。</li></ul></li><li>无实意 <ul><li>系动词：通过添加无实义的系动词构成主系表<sup class="footnote-ref"><a href="#footnote6">[6]</a><a class="footnote-anchor" id="footnote-ref6"></a></sup>结构。</li></ul></li></ul></li></ul><p>因为系动词不像实意动词一样具有实际意义，它存在与否都不影响句子的含义，只是充当谓语动词来保持句子完整性，我们通常将系动词分为下面四类：</p><ul><li>系动词的分类 <ul><li>be动词 单独使用</li><li>get become turn go grow 变得</li><li>look sound smell taste feel 感官动词</li><li>seem appear keep remain prove 看起来、似乎等不确定性</li></ul></li></ul><h3 id="句子结构" tabindex="-1"><a class="header-anchor" href="#句子结构" aria-hidden="true">#</a> 句子结构</h3><h4 id="主谓结构" tabindex="-1"><a class="header-anchor" href="#主谓结构" aria-hidden="true">#</a> 主谓结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. He cries. 
+他哭了。
+
+2. People arrived. 
+人们到了。
+
+3. Waiting lists increase all the time. 
+等候名单增加了，后面的时间是状语，并不影响组成完整的主谓句子。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="主谓宾结构" tabindex="-1"><a class="header-anchor" href="#主谓宾结构" aria-hidden="true">#</a> 主谓宾结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. We need progressive plocies.
+我们需要进一步的政策。
+
+2. I understand this.
+我理解这件事。
+
+3. This argument has attracted a lot of attention.
+这一论点引起起了广泛的关注。has attracted 两个动词组成时态作为一个动词构成主谓宾。
+
+4. Economic co-ordination should involve all 27 members of the EU clud.
+经济上的协作应该包含欧盟的27个成员国。should involve 是情态加谓语动词构成一个动词。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="主谓双宾结构" tabindex="-1"><a class="header-anchor" href="#主谓双宾结构" aria-hidden="true">#</a> 主谓双宾结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. It gave me the validation and confidence.
+它给了我认可和信心。
+
+2. Last month, Howard Schmidt offered the federal government a proposal.
+上个月，霍华德-施密特向联邦政府提出了一项建议。
+
+3. Last month,Howard Schmidt offered a proposal to the federal government.
+翻译同上，只不过将vt.+sb.+sth.变成了vt.+sth.+prep.+sb.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="主谓宾补结构" tabindex="-1"><a class="header-anchor" href="#主谓宾补结构" aria-hidden="true">#</a> 主谓宾补结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. Futhermore,these losses make us mature.
+此外，这些损失会让我们变得成熟。
+
+2. Pete Schneider considers the climb tougher today.
+皮特-施耐德认为今天的攀登更加艰难。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><blockquote><p>tips-直接宾语：动作的承受者，一般是物。<br> tips-间接宾语：动作的对谁、为谁做的，一般是人。</p></blockquote><h4 id="主系表结构" tabindex="-1"><a class="header-anchor" href="#主系表结构" aria-hidden="true">#</a> 主系表结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. Yet this understanding is outdate.
+然而这一理解是过时的。
+
+2. More education is the new principle.
+加强教育是新的原则。
+
+3. It was a really bad move.
+这真的是一个糟糕的举动。
+
+4. These purchases often become more valuable with time.
+随着时间的推移，这些采购通常会变得更有价值。
+
+5. Phones become our security blanket.
+电话成了我们的安全毯。
+
+6. The man looked puzzled and hurt.
+那人一脸疑惑和受伤。
+
+7. The zone&#39;s future looks uncertain at best.
+该区的未来充其量只能说是前途未卜。
+
+8. It remains the world&#39;s largest trading block.
+它依然是世界上最大的贸易区。
+
+9. But the discussions now seem out of date.
+但现在看来，这些讨论已经过时。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><hr class="footnotes-sep"><section class="footnotes"><ol class="footnotes-list"><li id="footnote1" class="footnote-item"><p>主语：句子中主要说明的对象是主语。 <a href="#footnote-ref1" class="footnote-backref">↩︎</a></p></li><li id="footnote2" class="footnote-item"><p>谓语：表述句子主语的内容叫谓语，因为汉语中谓的意思就是说话。 <a href="#footnote-ref2" class="footnote-backref">↩︎</a></p></li><li id="footnote3" class="footnote-item"><p>宾语：之所以叫宾语是因为它是句子中协作主语的部分，所以理解为宾客，叫做宾语。 <a href="#footnote-ref3" class="footnote-backref">↩︎</a></p></li><li id="footnote4" class="footnote-item"><p>主谓双宾：协作主语的宾语有时不仅仅是一个，而是两个，比如说：”我给你一本书。“这里的你和书就是两个宾语。 <a href="#footnote-ref4" class="footnote-backref">↩︎</a></p></li><li id="footnote5" class="footnote-item"><p>主谓宾补：在句子结构已经是主谓宾的情况下，发现句子含义并不完整，需要增加对宾语意义的补充，这里的补也就是宾语补足语的意思。 <a href="#footnote-ref5" class="footnote-backref">↩︎</a></p></li><li id="footnote6" class="footnote-item"><p>表语：用来修饰主语的，说明主语的身份、性质、品性、特征和状态的，表语位于系动词之后，与系动词联系十分紧密，有系动词就有表语，有表语就有系动词。 <a href="#footnote-ref6" class="footnote-backref">↩︎</a></p></li></ol></section>`,20),o=[l];function t(d,r){return i(),n("div",null,o)}const u=e(a,[["render",t],["__file","english.html.vue"]]);export{u as default};
