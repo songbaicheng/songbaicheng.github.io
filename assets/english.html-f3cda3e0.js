186b32e1bad3b84bbb64f4d9bc5d7c438dc2a365
songbaicheng <57879271+songbaicheng@users.noreply.github.com>
2023-07-29 03:50:30 +0000

    Deploying to gh-pages from @ songbaicheng/songbaicheng.github.io@710bd7a4b8ed3dcbe1fdbc8a18be226f304976e0 🚀

---
+++ assets/english.html-f3cda3e0.js
@@ -0,0 +1,104 @@
+import{_ as t}from"./plugin-vue_export-helper-c27b6911.js";import{r as l,o as d,c as r,a as e,d as i,w as a,e as s}from"./app-fb53f143.js";const o={},c=s(`<h1 id="english" tabindex="-1"><a class="header-anchor" href="#english" aria-hidden="true">#</a> English</h1><h2 id="简单句" tabindex="-1"><a class="header-anchor" href="#简单句" aria-hidden="true">#</a> 简单句</h2><p>我们从最简单的结构开始，一个句子只要有物质和运动就可以把一件事情最根本的要素描述出来，即 n. + v. 也就是一个名词和一个动词构成主<sup class="footnote-ref"><a href="#footnote1">[1]</a><a class="footnote-anchor" id="footnote-ref1"></a></sup>谓<sup class="footnote-ref"><a href="#footnote2">[2]</a><a class="footnote-anchor" id="footnote-ref2"></a></sup>，而且是一对一对应，一个主语对应一个谓语。在简单句的变化都是谓语的变化，至于如何变化我们得先知道谓语的分类</p><ul><li>谓语动词（v.）的分类 <ul><li>有实意 <ul><li>不及物动词（vi.）：主语独立完成，不用借助其他物就可以组成主谓的句子。</li><li>及物动词（vt.）：主语不能独立完成，需要借助宾语<sup class="footnote-ref"><a href="#footnote3">[3]</a><a class="footnote-anchor" id="footnote-ref3"></a></sup>来完成，构成主谓宾、主谓双宾<sup class="footnote-ref"><a href="#footnote4">[4]</a><a class="footnote-anchor" id="footnote-ref4"></a></sup>和主谓宾补<sup class="footnote-ref"><a href="#footnote5">[5]</a><a class="footnote-anchor" id="footnote-ref5"></a></sup>等结构。</li></ul></li><li>无实意 <ul><li>系动词：通过添加无实义的系动词构成主系表<sup class="footnote-ref"><a href="#footnote6">[6]</a><a class="footnote-anchor" id="footnote-ref6"></a></sup>结构。</li></ul></li></ul></li></ul><p>因为系动词不像实意动词一样具有实际意义，它存在与否都不影响句子的含义，只是充当谓语动词来保持句子完整性，我们通常将系动词分为下面四类：</p><ul><li>系动词的分类 <ul><li>be动词 单独使用</li><li>get become turn go grow 变得</li><li>look sound smell taste feel 感官动词</li><li>seem appear keep remain prove 看起来、似乎等不确定性</li></ul></li></ul><h3 id="句子结构" tabindex="-1"><a class="header-anchor" href="#句子结构" aria-hidden="true">#</a> 句子结构</h3><h4 id="主谓结构" tabindex="-1"><a class="header-anchor" href="#主谓结构" aria-hidden="true">#</a> 主谓结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. He cries. 
+他哭了。
+
+2. People arrived. 
+人们到了。
+
+3. Waiting lists increase all the time. 
+等候名单增加了，后面的时间是状语，并不影响组成完整的主谓句子。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="主谓宾结构" tabindex="-1"><a class="header-anchor" href="#主谓宾结构" aria-hidden="true">#</a> 主谓宾结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. We need progressive plocies.
+我们需要进一步的政策。
+
+2. I understand this.
+我理解这件事。
+
+3. This argument has attracted a lot of attention.
+这一论点引起起了广泛的关注。has attracted 两个动词组成时态作为一个动词构成主谓宾。
+
+4. Economic co-ordination should involve all 27 members of the EU clud.
+经济上的协作应该包含欧盟的27个成员国。should involve 是情态加谓语动词构成一个动词。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="主谓双宾结构" tabindex="-1"><a class="header-anchor" href="#主谓双宾结构" aria-hidden="true">#</a> 主谓双宾结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. It gave me the validation and confidence.
+它给了我认可和信心。
+
+2. Last month, Howard Schmidt offered the federal government a proposal.
+上个月，霍华德-施密特向联邦政府提出了一项建议。
+
+3. Last month,Howard Schmidt offered a proposal to the federal government.
+翻译同上，只不过将vt.+sb.+sth.变成了vt.+sth.+prep.+sb.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="主谓宾补结构" tabindex="-1"><a class="header-anchor" href="#主谓宾补结构" aria-hidden="true">#</a> 主谓宾补结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. Futhermore,these losses make us mature.
+此外，这些损失会让我们变得成熟。
+
+2. Pete Schneider considers the climb tougher today.
+皮特-施耐德认为今天的攀登更加艰难。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><blockquote><p>tips-直接宾语：动作的承受者，一般是物。<br> tips-间接宾语：动作的对谁、为谁做的，一般是人。</p></blockquote><h4 id="主系表结构" tabindex="-1"><a class="header-anchor" href="#主系表结构" aria-hidden="true">#</a> 主系表结构</h4><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. Yet this understanding is outdate.
+然而这一理解是过时的。
+
+2. More education is the new principle.
+加强教育是新的原则。
+
+3. It was a really bad move.
+这真的是一个糟糕的举动。
+
+4. These purchases often become more valuable with time.
+随着时间的推移，这些采购通常会变得更有价值。
+
+5. Phones become our security blanket.
+电话成了我们的安全毯。
+
+6. The man looked puzzled and hurt.
+那人一脸疑惑和受伤。
+
+7. The zone&#39;s future looks uncertain at best.
+该区的未来充其量只能说是前途未卜。
+
+8. It remains the world&#39;s largest trading block.
+它依然是世界上最大的贸易区。
+
+9. But the discussions now seem out of date.
+但现在看来，这些讨论已经过时。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="三态加一否" tabindex="-1"><a class="header-anchor" href="#三态加一否" aria-hidden="true">#</a> 三态加一否</h2><p>这里说的三态加一否指的是句子中谓语动词的变化，即时态、情态、语态和否定四种变化。</p><h3 id="时态部分" tabindex="-1"><a class="header-anchor" href="#时态部分" aria-hidden="true">#</a> 时态部分</h3><p>时态时态，即表达谓语的时间和状态，一共有16种时态：</p>`,22),u=e("thead",null,[e("tr",null,[e("th",{style:{"text-align":"center"}},"状态\\时间"),e("th",{style:{"text-align":"center"}},"过去"),e("th",{style:{"text-align":"center"}},"现在"),e("th",{style:{"text-align":"center"}},"将来"),e("th",{style:{"text-align":"center"}},"过去将来")])],-1),v=e("td",{style:{"text-align":"center"}},"一般",-1),h={style:{"text-align":"center"}},m=e("strong",null,[e("em",null,"一般过去时")],-1),b={style:{"text-align":"center"}},f=e("strong",null,[e("em",null,"一边现在时")],-1),p={style:{"text-align":"center"}},g=e("strong",null,[e("em",null,"一般将来时")],-1),x={style:{"text-align":"center"}},_=e("strong",null,[e("em",null,"一般过去将来时")],-1),y=e("td",{style:{"text-align":"center"}},"进行",-1),k={style:{"text-align":"center"}},w=e("strong",null,[e("em",null,"过去进行时")],-1),E={style:{"text-align":"center"}},T=e("strong",null,[e("em",null,"现在进行时")],-1),I=e("td",{style:{"text-align":"center"}},"将来进行时",-1),B=e("td",{style:{"text-align":"center"}},"过去将来进行时",-1),F=e("td",{style:{"text-align":"center"}},"完成",-1),N={style:{"text-align":"center"}},S=e("strong",null,[e("em",null,"过去完成时")],-1),q={style:{"text-align":"center"}},z=e("strong",null,[e("em",null,"现在完成时")],-1),A=e("td",{style:{"text-align":"center"}},"将来完成时",-1),H=e("td",{style:{"text-align":"center"}},"过去将来完成时",-1),P=e("tr",null,[e("td",{style:{"text-align":"center"}},"完成进行"),e("td",{style:{"text-align":"center"}},"过去完成进行时"),e("td",{style:{"text-align":"center"}},"现在完成进行时"),e("td",{style:{"text-align":"center"}},"将来完成进行时"),e("td",{style:{"text-align":"center"}},"过去将来完成进行时")],-1),j=s(`<blockquote><p>当然我们并不需要全部都掌握的炉火纯青，我们只要了解左上方的13种和重点掌握红色的八种最常用的时态即可。</p></blockquote><h3 id="一般" tabindex="-1"><a class="header-anchor" href="#一般" aria-hidden="true">#</a> 一般</h3><p>一般讲究的是这件事情不特殊，没有强调，我们掌握时态要掌握其形式和用法。</p><h4 id="一般过去时" tabindex="-1"><a class="header-anchor" href="#一般过去时" aria-hidden="true">#</a> 一般过去时</h4><p>一般过去时的形式就是谓语动词变成过去时，表示完全发生在过去的事情。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. The outbreak came to global notice in late April 2009.
+疫情于 2009 年 4 月下旬在全球范围内爆发。
+
+2. That was bad to my overall fitness goals.
+这对我的整体健身目标很不利。
+
+3. A century ago,the immigrants from across the Athantic included settlers and sojouners.
+一个世纪前，来自大西洋彼岸的移民包括定居者和商人。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="一般现在时" tabindex="-1"><a class="header-anchor" href="#一般现在时" aria-hidden="true">#</a> 一般现在时</h4><p>一般现在时表示发生在现在的事情，现在发生的事情有两种，一种是静态的状态，一种是动态的规律和习惯。形式有两种，一种是动词原形一种是第三人称单数，是否要使用第三人称单数需要配合主语来确定，一般动词的第三人称单数表示主语的个数是一个，这里要区分和名词的单复数的形式，第三人称单数是动词后面加s或者es表示一个，而名词复数则加s或者es表示多个。使用第三人称单数的有四种情况：他她它、可数名词单数、不可数名词和不定代词。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. However,a true cashless society is probably not around the corner.
+然而，真正的无现金社会可能还遥遥无期。
+
+2. I have an Excel template for a week of breakfast,lunch and dinner.
+我有一个一周早餐、午餐和晚餐的 Excel 模板。
+
+3. After bills,Tony has $60 a week to spend.
+除去账单，托尼每周有 60 美元的支出。
+
+4. Each book opens up new avenues of knowledeg.
+每本书都开辟了新的知识渠道。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="一般将来时" tabindex="-1"><a class="header-anchor" href="#一般将来时" aria-hidden="true">#</a> 一般将来时</h4><p>一般将来时表示现在的将来。形式有两种，一种是 will + do，多用于临时决定的打算；另一种是 am/is/are going to do，表示有计划要去做某事。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. It will hold another meeting soon.
+它将很快举行另一次会议。
+
+2. People will simply become lazy and depressed.
+人们很容易就会变得懒惰和消沉。
+
+3. Will the European Union make it.
+欧洲联盟能否做到这一点。
+
+4. The future will be wasteland of a different sort.
+未来将是另一种荒原。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="一般-过去将来时" tabindex="-1"><a class="header-anchor" href="#一般-过去将来时" aria-hidden="true">#</a> （一般）过去将来时</h4><p>对比于一般将来时，表示的是过去的将来。形式也变成了 would do 或者 was/were going to do。因为要告知是过去的状态，所以一般会搭配一般过去时使用。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. Facebook promiesd the European commission…… It would not link phone numbers to Facebook identities.
+Facebook 向欧盟委员会承诺......，不会将电话号码与 Facebook 的身份联系起来。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="进行状态" tabindex="-1"><a class="header-anchor" href="#进行状态" aria-hidden="true">#</a> 进行状态</h3><p>进行状态表示正在进行或者是持续进行的状态。进行时态有统一的形式，都是用 be + doing 来表示，只不过这里的 be 会根据过去、现在和将来换成不同的词态。</p><h4 id="过去进行时" tabindex="-1"><a class="header-anchor" href="#过去进行时" aria-hidden="true">#</a> 过去进行时</h4><p>表示过去正在进行的事情。形式是 was/were doing。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. The position was just taking up too much time,she said.
+她说，这个职位占用了太多时间。
+
+2. But 10 yeas ago he was earning $130,000 a year.
+但 10 年前，他的年收入是 13 万美元。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="现在进行时" tabindex="-1"><a class="header-anchor" href="#现在进行时" aria-hidden="true">#</a> 现在进行时</h4><p>表示现在正在进行的事情。形式是 am/is/are doing。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. But this era of high joblessness is probably beginning.
+但是，这个高失业率时代可能已经开始。
+
+2. Now shortages are appearing at the mid-skill levels.
+现在，中等技能水平的人员也出现短缺。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="将来进行时" tabindex="-1"><a class="header-anchor" href="#将来进行时" aria-hidden="true">#</a> 将来进行时</h4><p>表示未来在进行的事情。形式是 will be doing。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1. Native U.S. workers won&#39;t be returning to the farm.
+美国本地工人不会返回农场。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="完成状态" tabindex="-1"><a class="header-anchor" href="#完成状态" aria-hidden="true">#</a> 完成状态</h3><p>这里的完成并不代表都做完了，也可以表示可以部分完成，英语的完成时态主要是表示一件事的完成结果，即从某个时间点之前的完成结果。统一的形式就是 hava + done。</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h4 id="过去完成时" tabindex="-1"><a class="header-anchor" href="#过去完成时" aria-hidden="true">#</a> 过去完成时</h4><p>表达过去的之前完成的一件事的结果叫过去完成时。一般结构为 had + done 组成。</p><hr class="footnotes-sep"><section class="footnotes"><ol class="footnotes-list"><li id="footnote1" class="footnote-item"><p>主语：句子中主要说明的对象是主语。 <a href="#footnote-ref1" class="footnote-backref">↩︎</a></p></li><li id="footnote2" class="footnote-item"><p>谓语：表述句子主语的内容叫谓语，因为汉语中谓的意思就是说话。 <a href="#footnote-ref2" class="footnote-backref">↩︎</a></p></li><li id="footnote3" class="footnote-item"><p>宾语：之所以叫宾语是因为它是句子中协作主语的部分，所以理解为宾客，叫做宾语。 <a href="#footnote-ref3" class="footnote-backref">↩︎</a></p></li><li id="footnote4" class="footnote-item"><p>主谓双宾：协作主语的宾语有时不仅仅是一个，而是两个，比如说：”我给你一本书。“这里的你和书就是两个宾语。 <a href="#footnote-ref4" class="footnote-backref">↩︎</a></p></li><li id="footnote5" class="footnote-item"><p>主谓宾补：在句子结构已经是主谓宾的情况下，发现句子含义并不完整，需要增加对宾语意义的补充，这里的补也就是宾语补足语的意思。 <a href="#footnote-ref5" class="footnote-backref">↩︎</a></p></li><li id="footnote6" class="footnote-item"><p>表语：用来修饰主语的，说明主语的身份、性质、品性、特征和状态的，表语位于系动词之后，与系动词联系十分紧密，有系动词就有表语，有表语就有系动词。 <a href="#footnote-ref6" class="footnote-backref">↩︎</a></p></li></ol></section>`,33);function U(V,W){const n=l("font");return d(),r("div",null,[c,e("table",null,[u,e("tbody",null,[e("tr",null,[v,e("td",h,[i(n,{color:"red"},{default:a(()=>[m]),_:1})]),e("td",b,[i(n,{color:"red"},{default:a(()=>[f]),_:1})]),e("td",p,[i(n,{color:"red"},{default:a(()=>[g]),_:1})]),e("td",x,[i(n,{color:"red"},{default:a(()=>[_]),_:1})])]),e("tr",null,[y,e("td",k,[i(n,{color:"red"},{default:a(()=>[w]),_:1})]),e("td",E,[i(n,{color:"red"},{default:a(()=>[T]),_:1})]),I,B]),e("tr",null,[F,e("td",N,[i(n,{color:"red"},{default:a(()=>[S]),_:1})]),e("td",q,[i(n,{color:"red"},{default:a(()=>[z]),_:1})]),A,H]),P])]),j])}const $=t(o,[["render",U],["__file","english.html.vue"]]);export{$ as default};
