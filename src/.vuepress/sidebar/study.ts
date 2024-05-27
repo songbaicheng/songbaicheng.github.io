a3a6bd4101c15611e016353e9272387a054a0246
songbaicheng <songbaicheng16@163.com>
2024-05-27 16:49:32 +0800

    feat: add code trick.

--- src/.vuepress/sidebar/study.ts
+++ src/.vuepress/sidebar/study.ts
@@ -91,7 +91,8 @@ export const studySidebar = arraySidebar([
         collapsible: true,
         children: [
             '/study/design-pattern/README.md',
-            '/study/design-pattern/observer.md'
+            '/study/design-pattern/observer.md',
+            '/study/design-pattern/chain.md'
         ]
     },
     {
---
+++ src/study/design-pattern/chain.md
@@ -0,0 +1,9 @@
+---
+category: 设计模式
+tag: 
+  - chain pattern
+  - Java
+  - Spring Boot
+---
+
+# 责任链模式
