1931f0b3b8c18508f48bbfbb9f6dc20f1fdeb26d
songbaicheng <songbaicheng16@163.com>
2024-06-25 00:29:28 +0800

    Optimize Redis description and add principle analysis and scenario applications.

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -2,11 +2,9 @@ import { sidebar } from "vuepress-theme-hope";
 import { studySidebar } from "./study"
 import { workTaskSidebar } from "./work-task"
 import { resourceSidebar } from "./resource"
-import { hobbiesSidebar } from "./hobbies"
 
 export const Sidebar = sidebar({
   '/study/': studySidebar,
   '/work-task/': workTaskSidebar,
   '/resource/': resourceSidebar,
-  '/hobbies/': hobbiesSidebar,
 });
