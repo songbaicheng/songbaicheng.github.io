c1955988a38779425af3f526f18cf5839a32b4dd
songbaicheng <songbaicheng@sdpjw.com>
2024-12-07 14:13:33 +0800

    新增ai部分

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,6 +4,7 @@ export const Navbar = navbar([
     "/",
     {text: "学习之路", icon: "study", link: "/study/"},
     {text: "工作任务", icon: "workingDirectory", link: "/work-task/"},
+    {text: "AI 领域", icon: '/icon/ai.svg', link: "/ai/"},
     {text: "资源分享", icon: "box", link: "/resource/"},
     {
         text: "过去", icon: "time", children: [{
