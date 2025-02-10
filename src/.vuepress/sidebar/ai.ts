014d5e32aa91ea89a47afb807019e7069f9fe7a7
songbaicheng <songbaicheng@sdpjw.com>
2025-02-10 17:51:56 +0800

    更新目录

--- src/.vuepress/sidebar/ai.ts
+++ src/.vuepress/sidebar/ai.ts
@@ -16,19 +16,12 @@ export const aiSidebar = arraySidebar([
         children: [
             '/ai/deep-learning/README.md',
             '/ai/deep-learning/pytorch.md',
-        ]
-    },
-    {
-        text: 'Hugging-Face',
-        icon: '/assets/images/ai/hugging-face/huggingface.svg',
-        collapsible: true,
-        children: [
-            '/ai/hugging-face/README.md',
+            '/ai/deep-learning/hugging-face.md',
         ]
     },
     {
         text: 'Agent',
-        icon: '/assets/images/ai/hugging-face/huggingface.svg',
+        icon: '/assets/images/ai/agent/agent-logo.svg',
         collapsible: true,
         children: [
             '/ai/agent/README.md',
--- src/ai/deep-learning/hugging-face.md
+++ src/ai/deep-learning/hugging-face.md
@@ -3,7 +3,7 @@
 ```card
 title: Hugging Face 课堂官网
 desc: 点击跳转 Hugging Face 查看详细内容
-logo: /assets/images/ai/hugging-face/huggingface_logo.svg
+logo: /assets/images/ai/deep-learning/hugging-face/huggingface_logo.svg
 link: https://huggingface.co/learn
 color: rgba(173, 216, 590, 0.15)
 ```
