602d9e240eb46b7874b01a7fac7edd3bb587f84a
songbaicheng <songbaicheng@sdpjw.com>
2025-02-19 16:06:33 +0800

    更新AI文档内容及图标

--- src/.vuepress/sidebar/ai.ts
+++ src/.vuepress/sidebar/ai.ts
@@ -2,21 +2,22 @@ import { arraySidebar } from "vuepress-theme-hope";
 
 export const aiSidebar = arraySidebar([
     {
+        text: '大语言模型',
+        icon: '/assets/images/ai/llm/llm.svg',
+        collapsible: true,
+        children: [
+            '/ai/llm/README.md',
+            '/ai/llm/deep-learning.md',
+            '/ai/llm/pytorch.md',
+            '/ai/llm/hugging-face.md',
+        ]
+    },
+    {
         text: 'AI 应用',
         icon: '/assets/images/ai/application/prompt/prompt.svg',
         collapsible: true,
         children: [
             '/ai/application/prompt-engineering.md',
-        ]
-    },
-    {
-        text: '深度学习',
-        icon: '/assets/images/ai/llm/llm.svg',
-        collapsible: true,
-        children: [
-            '/ai/llm/deep-learning.md',
-            '/ai/llm/pytorch.md',
-            '/ai/llm/hugging-face.md',
         ]
     },
 ])
